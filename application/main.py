52b0eb8c7acaac34bc3f552010524adc2d6de35e
Fire4Dev <fire4dev@gmail.com>
2019-11-19 23:44:20 -0300

    Added new archives
    Signed-off-by: Fire4Dev <fire4dev@gmail.com>

---
+++ application/main.py
@@ -0,0 +1,5 @@
+# @@@@@ IMPORTS @@@@@@
+import random
+import time
+from models import users_model
+from datetime import datetime
---
+++ models/users_model.py
@@ -0,0 +1,8 @@
+# @@@@@ IMPORTS @@@@@@
+from configs import bd
+
+# here are the area os users (normal user and admin)
+# some basic informations
+
+def user(self):
+    bd.cursor.execut('')
